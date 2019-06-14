6cedce6337222d66825b3e96dede048623855aeb
Adam N <51328998+adamn-hmrc@users.noreply.github.com>
2019-06-14 12:41:55 +0100

    Remove AWS integration

--- request_ssh_access/config.py
+++ request_ssh_access/config.py
@@ -1,11 +1,16 @@
 import os
 
 COMMAND_TEMPLATE = """
-Email, slack or text or pigeon-mail to an authorised person:
+(Note that this is for convenience only and you may modify these values to whatever you need)
+
+Please email, slack, text, or pigeon-mail to an authorised person the following command: 
+
 `aws lambda do-it --user-name={user_name} --public-key="{public_key}" --ttl={ttl}`
+
 """
 
 DEFAULT_TTL = 60 * 60 * 8  # 8 hours
+DEFAULT_PUBKEY_PATH = os.path.expanduser("~/.ssh/id_rsa.pub")
 DEFAULT_CERT_PATH = os.path.expanduser("~/.ssh/id_rsa-cert.pub")
 TEMPORARY_TOKEN = "s.mpFPfSSRNnCj2VVUbSfOhTj3"
 DEFAULT_PROFILE_NAME = "webops-users"
--- request_ssh_access/request_ssh_access.py
+++ request_ssh_access/request_ssh_access.py
@@ -2,20 +2,15 @@
 
 """
 TODO:
-- SQS/S3/Lambda integration - read the token from the user's queue
-- Check for wrapped tokens on the user's queue at startup.
-- Error handling for Everything
+- Error Handling for Everything
 - setup.py
-- account IDs
+- ci pipeline
 """
 
 import argparse
 import getpass
-import json
 import logging
-import time
 
-from mfa_session import MFASession
 import config
 import vault
 
@@ -23,7 +18,6 @@ logging.basicConfig(
     level=logging.WARN, format="%(asctime)s %(levelname)-5s %(message)s"
 )
 
-logging.getLogger("boto").setLevel(logging.WARN)
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +25,9 @@ logger = logging.getLogger(__name__)
 def main():
     parser = argparse.ArgumentParser(
         description="Helper utility to create Vault-signed SSH certificates"
+    )
+    parser.add_argument(
+        "--user-name", help="AWS/LDAP user name", type=str, required=True
     )
     parser.add_argument(
         "--environment",
@@ -47,6 +44,15 @@ def main():
         ],
     )
     parser.add_argument(
+        "--ssh-public-key",
+        help="Path to read ssh public key from (default: '{}')".format(
+            config.DEFAULT_PUBKEY_PATH
+        ),
+        default=config.DEFAULT_PUBKEY_PATH,
+        type=str,
+    )
+
+    parser.add_argument(
         "--output-ssh-cert",
         help="Path to write signed certificate (default: '{}')".format(
             config.DEFAULT_CERT_PATH
@@ -57,26 +63,27 @@ def main():
 
     args = parser.parse_args()
 
-    session = MFASession(profile_name=config.DEFAULT_PROFILE_NAME)
-    session.mfa_login()
+    public_key = get_public_key(public_key_path=args.ssh_public_key)
 
-    public_key = get_public_key(session)
+    print_lambda_command_to_copy(public_key, args.user_name)
 
-    print_lambda_command_to_copy(public_key, session.user_name)
+    wrapped_token = input(
+        "Enter the Vault wrapped token you received back from the authorised user: "
+    )
+    wrapped_token = wrapped_token.strip(" '\"")
 
-    input("Press Enter to continue...")
-
-    wrapped_token = get_wrapped_secret_from_sqs(session)
-
-    prompt = "LDAP password for '{user}' in '{env}': ".format(
-        user=session.user_name, env=args.environment
+    prompt = (
+        "Now we're ready to unwrap the signed certificate for you.\n"
+        "Please enter the LDAP password for '{user}' in '{env}': ".format(
+            user=args.user_name, env=args.environment
+        )
     )
 
     ldap_password = getpass.getpass(prompt=prompt)
     try:
         unwrapped_cert = vault.unwrap(
             args.environment,
-            vault.login(args.environment, session.user_name, ldap_password),
+            vault.login(args.environment, args.user_name, ldap_password),
             wrapped_token,
         )
     except:
@@ -84,21 +91,19 @@ def main():
         raise
 
     write_cert_to_file(args.output_ssh_cert, unwrapped_cert)
-    print("you are now authorised to log in.")
-
-
-def get_public_key(session):
-    ssh_keys = session.session.client("iam").list_ssh_public_keys(
-        UserName=session.user_name
-    )
-    ssh_keys = [k for k in ssh_keys["SSHPublicKeys"] if k["Status"] == "Active"]
-    ssh_key_id = ssh_keys.pop()["SSHPublicKeyId"]
-
-    response = session.session.client("iam").get_ssh_public_key(
-        UserName=session.user_name, SSHPublicKeyId=ssh_key_id, Encoding="SSH"
+    print(
+        "\nyou are now authorised to log in using the following command: \n"
+        'ssh -i {} -i {} "${{REMOTE_HOST}}"\n'.format(
+            args.output_ssh_cert, args.ssh_public_key
+        )
     )
 
-    return response["SSHPublicKey"]["SSHPublicKeyBody"]
+
+def get_public_key(public_key_path):
+    with open(public_key_path, "r") as f:
+        # Clean up public key here:
+        # "ssh-rsa AAB...3odo3Xsjd user@host\n" -> "ssh-rsa AAB...3odo3Xsjd"
+        return " ".join(f.readline().rstrip().split(" ")[:2])
 
 
 def print_lambda_command_to_copy(public_key, user_name):
@@ -113,37 +118,6 @@ def write_cert_to_file(output_ssh_cert, unwrapped_cert):
     with open(output_ssh_cert, "w") as f:
         f.write(unwrapped_cert)
     print("signed certificate written to '{}'.".format(output_ssh_cert))
-
-
-def get_wrapped_secret_from_sqs(session):
-    credentials = session.assume_role()
-    sqs = session.session.client(
-        "sqs",
-        aws_access_key_id=credentials["AccessKeyId"],
-        aws_secret_access_key=credentials["SecretAccessKey"],
-        aws_session_token=credentials["SessionToken"],
-    )
-
-    queue_url_response = sqs.get_queue_url(
-        QueueName="adam-test-13-june-2019", QueueOwnerAWSAccountId="132732819912"
-    )
-    queue_url = queue_url_response["QueueUrl"]
-
-    message_response = get_message_from_queue(queue_url, sqs)
-    return json.loads(message_response)['token']
-
-
-
-def get_message_from_queue(queue_url, sqs):
-    while True:
-        print("waiting for message...")
-        response = sqs.receive_message(
-            QueueUrl=queue_url
-        )
-        if response.get('Messages', []):
-            return response['Messages'].pop()['Body']
-
-        time.sleep(10)
 
 
 if __name__ == "__main__":
