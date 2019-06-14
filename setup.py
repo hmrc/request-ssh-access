b13cfaadb481b35e13e50d68800f9a772d249340
Adam N <51328998+adamn-hmrc@users.noreply.github.com>
2019-06-14 15:48:48 +0100

    Fix up setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ from codecs import open
 import os
 
 
-requires = ["requests"]
-
-
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
@@ -21,14 +18,7 @@ setup(
     long_description=read("README.md"),
     platforms=["Linux", "MacOS"],
     packages=find_packages(),
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=requires,
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+    install_requires=["requests"],
     entry_points={
         "console_scripts": ["request-ssh-access = request_ssh_access.__main__:main"]
     },
