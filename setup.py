c3034c8be07a8b3c228c359221911a139930dc62
Adam N <51328998+adamn-hmrc@users.noreply.github.com>
2019-07-23 14:15:45 +0100

    PSEC-655 Output xunit report instead of coverage

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=read("README.md"),
     platforms=["Linux", "MacOS"],
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=["requests>=2.20.0"],
     entry_points={
         "console_scripts": ["request-ssh-access = request_ssh_access.__main__:main"]
     },
