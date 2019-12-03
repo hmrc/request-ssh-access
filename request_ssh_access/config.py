212f1e55bde0f3042717484e13076be28de9d75f
Richard Fortescue-Webb <rfwebb@gmail.com>
2019-12-03 09:38:59 +0000

    Update docs for console approach

--- request_ssh_access/config.py
+++ request_ssh_access/config.py
@@ -3,7 +3,18 @@ import os
 COMMAND_TEMPLATE = """
 (Note that this is for convenience only and you may modify these values to whatever you need)
 
-Please email, slack, text, or pigeon-mail to an authorised person the following command:
+Please email, slack, text, or pigeon-mail to an authorised person the following JSON:
+```
+{
+    "user_name": "{user_name}",
+    "ttl": {ttl}
+}
+```
+
+Send them the following link for documentation:
+https://github.com/hmrc/grant-ssh-access/blob/master/README.md#instructions-for-granting-ssh-access-to-an-engineer
+
+Or if the person has AWS CLI setup, they can run the following
 
 aws --profile=platform_owner lambda invoke --function-name {function_arn} --payload "{{\\"user_name\\": \\"{user_name}\\", \\"ttl\\": \\"{ttl}\\"}}" /tmp/grant_outfile && cat /tmp/grant_outfile
 
