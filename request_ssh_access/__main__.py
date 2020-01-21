ebabbc9f29562dcf636129dfe8858683754f17f7
j450nb <j450nb@users.noreply.github.com>
2020-01-21 11:58:05 +0000

    INFRA-6468 Wrap main in a try/except

--- request_ssh_access/__main__.py
+++ request_ssh_access/__main__.py
@@ -17,66 +17,70 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    PROMPT = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 def main(args=None):
-    class bcolors:
-        HEADER = "\033[95m"
-        OKBLUE = "\033[94m"
-        OKGREEN = "\033[92m"
-        PROMPT = "\033[93m"
-        FAIL = "\033[91m"
-        ENDC = "\033[0m"
-        BOLD = "\033[1m"
-        UNDERLINE = "\033[4m"
+    try:
+        if args is None:
+            args = sys.argv[1:]
+        args = parse_args(args)
 
-    if args is None:
-        args = sys.argv[1:]
-    args = parse_args(args)
+        if args.ttl > config.MAX_TTL:
+            max_ttl_message = (
+                bcolors.FAIL
+                + f"FAILED: The TTL requested is greater than MAX_TTL which is set {config.MAX_TTL}\n"
+                + bcolors.ENDC
+            )
+            raise Exception(max_ttl_message)
 
-    if args.ttl > config.MAX_TTL:
-        sys.tracebacklimit = -1
-        max_ttl_message = (
-            bcolors.FAIL
-            + f"FAILED: The TTL requested is greater than MAX_TTL which is set {config.MAX_TTL}\n"
+        print_lambda_command_to_copy(args.user_name, args.environment, ttl=args.ttl)
+
+        wrapped_token = get_input(
+            bcolors.PROMPT
+            + "Enter the Vault wrapped token you received back from the authorised user: "
             + bcolors.ENDC
         )
-        raise Exception(max_ttl_message)
+        wrapped_token = wrapped_token.strip(" '\"")
 
-    print_lambda_command_to_copy(args.user_name, args.environment, ttl=args.ttl)
+        prompt = (
+            "Now we're ready to unwrap the signed certificate for you.\n"
+            + bcolors.PROMPT
+            + f"Please enter the LDAP password for {args.user_name} in '{args.environment}': "
+            + bcolors.ENDC
+        )
 
-    wrapped_token = get_input(
-        bcolors.PROMPT
-        + "Enter the Vault wrapped token you received back from the authorised user: "
-        + bcolors.ENDC
-    )
-    wrapped_token = wrapped_token.strip(" '\"")
+        ldap_password = getpass.getpass(prompt=prompt)
+        unwrapped_cert = vault.unwrap(
+            args.environment,
+            vault.login(args.environment, args.user_name, ldap_password),
+            wrapped_token,
+        )
 
-    prompt = (
-        "Now we're ready to unwrap the signed certificate for you.\n"
-        + bcolors.PROMPT
-        + f"Please enter the LDAP password for {args.user_name} in '{args.environment}': "
-        + bcolors.ENDC
-    )
-
-    ldap_password = getpass.getpass(prompt=prompt)
-    unwrapped_cert = vault.unwrap(
-        args.environment,
-        vault.login(args.environment, args.user_name, ldap_password),
-        wrapped_token,
-    )
-
-    write_cert_to_file(get_output_cert_path(args), unwrapped_cert)
-    print(
-        "\nyou are now authorised to log in using the following command: \n",
-        'ssh -o "IdentityAgent none" -i {} -i {} "${{REMOTE_HOST}}"\n'.format(
-            args.input_ssh_cert, get_output_cert_path(args)
-        ),
-        "\nor add the following to the appropriate Hosts section in your ~/.ssh/confg\n",
-        "\n\tUser {}".format(args.user_name),
-        "\n\tIdentitiesOnly yes",
-        "\n\tIdentityFile {}".format(args.input_ssh_cert),
-        "\n\nand then log in with the following command: \n",
-        'ssh "${{REMOTE_HOST}}"\n',
-    )
+        write_cert_to_file(get_output_cert_path(args), unwrapped_cert)
+        print(
+            "\nyou are now authorised to log in using the following command: \n",
+            'ssh -o "IdentityAgent none" -i {} -i {} "${{REMOTE_HOST}}"\n'.format(
+                args.input_ssh_cert, get_output_cert_path(args)
+            ),
+            "\nor add the following to the appropriate Hosts section in your ~/.ssh/confg\n",
+            "\n\tUser {}".format(args.user_name),
+            "\n\tIdentitiesOnly yes",
+            "\n\tIdentityFile {}".format(args.input_ssh_cert),
+            "\n\nand then log in with the following command: \n",
+            'ssh "${{REMOTE_HOST}}"\n',
+        )
+    except Exception as e:
+        print(e)
+        exit(1)
 
 
 def parse_args(argv):
